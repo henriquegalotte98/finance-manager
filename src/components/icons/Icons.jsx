84b4d97634a843c7b7ff28aa61d7fa7b9999babc
henriquegalotte98 <galottehenrique@gmail.com>
2026-02-21 11:41:00 -0300

    Ajustando de acordo com o vite os caminhos das imagens

--- src/components/icons/Icons.jsx
+++ src/components/icons/Icons.jsx
@@ -1,7 +1,7 @@
-export const LogoIcon = <img src="public\area-chart-svgrepo-com.svg" alt="Logo" className="icons"/>;
-export const HomeIcon = <img src="public\home-svgrepo-com.svg" alt="Home"className="icons" />;
-export const PlanilhaIcon = <img src="public\file-excel-svgrepo-com.svg" alt="Planilha" className="icons"/>;
-export const EconomiasIcon = <img src="public\money-transfer-svgrepo-com.svg" alt="Economias" className="icons"/>;
-export const DolarIcon = <img src="public\chart-column-svgrepo-com.svg" alt="Dólar" className="icons"/>;
-export const ViagensIcon = <img src="public\travel-luggage-svgrepo-com.svg" alt="Viagens" className="icons"/>;
-export const ConfigIcon = <img src="public\config-svgrepo-com.svg" alt="Configurações" className="icons"/>;
+export const LogoIcon = <img src="\area-chart-svgrepo-com.svg" alt="Logo" className="icons"/>;
+export const HomeIcon = <img src="\home-svgrepo-com.svg" alt="Home"className="icons" />;
+export const PlanilhaIcon = <img src="\file-excel-svgrepo-com.svg" alt="Planilha" className="icons"/>;
+export const EconomiasIcon = <img src="\money-transfer-svgrepo-com.svg" alt="Economias" className="icons"/>;
+export const DolarIcon = <img src="\chart-column-svgrepo-com.svg" alt="Dólar" className="icons"/>;
+export const ViagensIcon = <img src="\travel-luggage-svgrepo-com.svg" alt="Viagens" className="icons"/>;
+export const ConfigIcon = <img src="\config-svgrepo-com.svg" alt="Configurações" className="icons"/>;
