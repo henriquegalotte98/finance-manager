180e06f138544876a3172994c8539f9832a3140b
henriquegalotte98 <galottehenrique@gmail.com>
2026-03-26 10:23:59 -0300

    Cors config

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,19 @@ app.use(cors({
 
 app.options("*", cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use("/expenses", expensesRoutes);
 app.use("/couple", coupleRoutes);
